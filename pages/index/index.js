5ee18ecf70dd1ccc2e06b226b74916b3874e0288
Wang, Joice <ligin_wang@yeah.net>
2019-01-20 22:18:01 +0800

    modify

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,79 @@
+var utils = require("../../utils/util.js")
 //index.js
 //获取应用实例
 const app = getApp()
 
+//用户登陆
+function userLogin() {
+  wx.checkSession({
+    success: function () {
+      //存在登陆态
+    },
+    fail: function () {
+      //不存在登陆态
+      onLogin()
+    }
+  })
+}
+const weburl = "http://digitaltmc-digitaltmc1.7e14.starter-us-west-2.openshiftapps.com/digitaltmc/";
+
+function onLogin() {
+  wx.login({
+    success: function (res) {
+      if (res.code) {
+        //发起网络请求
+        wx.request({
+          url: weburl + "auth",
+          data: {
+            code: res.code
+          },
+          success: function (res) {
+            const self = this
+            //console.log(res);
+            if (res.data) {
+              //获取到用户凭证 存儲 3rd_session 
+              //var json = JSON.parse(res.data.Data)
+              wx.setStorage({
+                key: "third_Session",
+                data: res.data
+                //data: json.third_Session
+              })
+              //getUserInfo()
+            }
+            else {
+
+            }
+          },
+          fail: function (res) {
+
+          }
+        })
+      }
+    },
+    fail: function (res) {
+
+    }
+  })
+
+}
+
 Page({
   data: {
     motto: 'Hello World',
+    role_data: ["TMD", "TTM", "TTIE"],
     userInfo: {},
     hasUserInfo: false,
+    test: "test",
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
-      // url: '../logs/logs'
-      url: '../booker/booker'
+      url: '../logs/logs'
     })
   },
   onLoad: function () {
+    app.getBookingInfo;
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -51,9 +108,31 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }, onGotUserInfo: function (e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.userInfo)
-    console.log(e.detail.rawData)
+  },
+  navtotable: function(e){
+    wx.navigateTo({
+      url: '../booker/booker'
+    })
+  },
+  handleUserInfo(e) {
+    console.log("aaa");
+    onLogin();
+    // let msg = e.detail.errMsg;
+    // if (msg === 'getUserInfo:ok') {
+    //   let user = e.detail.userInfo;
+    //   user.encryptedData = e.detail.encryptedData;
+    //   user.iv = e.detail.iv;
+    //   utils.setDataBase('islogin', true);//set 本地数据
+    //   utils.login(user, this.handleLogin.bind(this))
+    //   this.setData({
+    //     islogin: false
+    //   });
+    // } else {
+    //   utils.setDataBase('islogin', false);//set 本地数据
+    //   this.setData({
+    //     islogin: true
+    //   });
+    //   utils.showModal('五色糖，申请获得您的公开信息（头像，昵称等）。授权后，您能体验到我们更完善的功能，谢谢您关注五色糖。', false, this);
+    // }
   }
 })
